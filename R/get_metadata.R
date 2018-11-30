86bd669bd3bc3c9fb5e0817f3c94db6360e7dd4c
obrl_soil <obrl-soil@users.noreply.github.com>
2018-11-30 22:03:42 +1000

    Create get_metadata.R
    todo: test

---
+++ R/get_metadata.R
@@ -0,0 +1,89 @@
+#' download SLGA soils metadata
+#'
+#' Retrieves metadata from Soil and Landscape Grid of Australia soils WCS
+#' endpoints in XML or list format.
+#'
+#' @param product Character, one of the options from column 'Short_Name' in
+#'   \code{\link[slga:slga_product_info]{slga_product_info}}.
+#' @param attribute Character, one of the options from column 'Code' in
+#'   \code{\link[slga:slga_attribute_info]{slga_attribute_info}}, where 'Type' =
+#'   'Soil'.
+#' @param component Character, one of 'value', 'ci_low', or 'ci_high'.
+#' @param depth Integer, a number from 1 to 6. The numbers correspond to the
+#'   following depth ranges:
+#'  \enumerate{
+#'   \item 0 to 5 cm.
+#'   \item 5 to 15 cm.
+#'   \item 15 to 30 cm.
+#'   \item 30 to 60 cm.
+#'   \item 60 to 100 cm.
+#'   \item 100 to 200 cm.
+#'   }
+#' @param req_type Character; one of 'cap' or 'desc'. Defaults to 'desc'.
+#' @param format Character; one of 'xml' or 'native'. Defaults to 'native'.
+#' @return A list or xml document object, depending on the value of 'format'.
+#' @note WCS services < v 2.0 can only return XML formatted data; JSON is not an
+#'   option.
+#' @ImportFrom httr GET content
+#' @importFrom xml2 as_list
+#'
+metadata_soils <- function(product = NULL, attribute = NULL,
+                           component= NULL, depth = NULL,
+                           req_type = 'desc', format = 'native') {
+
+  req_type = match.arg(req_type, c('cap' , 'desc'))
+  this_url <- switch(req_type,
+         'cap' = get_soils_url(product = product, attribute = attribute,
+                               req_type = 'cap'),
+         'desc' = get_soils_url(product = product, attribute = attribute,
+                                component = component, depth = depth,
+                                req_type = 'desc'))
+
+  this_metadata <- httr::GET(url = this_url) # WCS < 2.0 won't return JSON ;_;
+
+  if(format = 'native') {
+    xml2::as_list(content(this_metadata))
+  } else {
+    content(this_metadata)
+  }
+}
+
+#' download SLGA landscape metadata
+#'
+#' Retrieves metadata from Soil and Landscape Grid of Australia landscape WCS
+#' endpoints in XML or list format.
+#'
+#' @param product Character, one of the options from column 'Short_Name' in
+#'   \code{\link[slga:slga_product_info]{slga_product_info}}, where 'Type' =
+#'   'Landscape'.
+#' @param req_type Character; one of 'cap' or 'desc'. Defaults to 'desc'.
+#' @param format Character; one of 'xml' or 'native'. Defaults to 'native'.
+#' @return A list or xml document object, depending on the value of 'format'.
+#' @note \itemize{
+#'   \item{WCS services < v 2.0 can only return XML formatted data; JSON is not an
+#'   option.}
+#'   \item{Parameter `product` is optional for `req_type = 'desc'`, leave out to
+#'   get metadata for all available landscape products.}
+#'   }
+#' @ImportFrom httr GET content
+#' @importFrom xml2 as_list
+#' @export
+#'
+metadata_lscape <- function(product  = NULL,
+                            req_type = 'desc',
+                            format   = 'native') {
+
+  req_type = match.arg(req_type, c('cap' , 'desc'))
+  this_url <-
+    switch(req_type,
+           'cap'  = get_soils_url(product = product, req_type = 'cap'),
+           'desc' = get_soils_url(product = product, req_type = 'desc'))
+
+  this_metadata <- httr::GET(url = this_url) # WCS < 2.0 won't return JSON ;_;
+
+  if(format = 'native') {
+    xml2::as_list(content(this_metadata))
+  } else {
+    content(this_metadata)
+  }
+}
